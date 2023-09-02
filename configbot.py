6509bff7368823ece39561311245b399d2a24d65
Basepy <143889306+Basepy@users.noreply.github.com>
2023-09-02 11:43:11 -0300

    Update configbot.py

--- configbot.py
+++ configbot.py
@@ -1,17 +1,17 @@
 
 #inserir token do bot entre as aspas
-bot_principal_token = "5890227925:AAGGmy-BFxHiQwdGtjB8wvB-3JT5Sh47Ln0"
+bot_principal_token = "TOKEN_TELEGRAM"
 
 
 #Configuração do texto INFORMAÇÔES
 msg_txt01 = "Informações Gerais"   #escrever o texto entre as Aspas
 msg_txt02 = "Olá, meu nome é X, e estou há 05 anos trabalhando como P2P na área."   #escrever o texto entre as Aspas
 msg_txt03 = "Minha taxa de transação é X, e etc..."   #escrever o texto entre as Aspas
-contato_telegram = "@Basesuporte"   #insira seu @username do telegram entre as Aspas
+contato_telegram = "@SATOSHINAKAMOTO"   #insira seu @username do telegram entre as Aspas
 
 #social midia
 nome_rede_social = "Twitter"   #escreva o nome da rede social que deseja usar para contato
-link_redesocial = "https://twitter.com/basebitcoin_"    #adicione o link da rede social
+link_redesocial = "https://twitter.com/basepy_"    #adicione o link da rede social
 
 
 
